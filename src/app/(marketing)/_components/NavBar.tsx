46261a23e8e8c057a9defe13285567143faae40c
hcheung-code <200518735+hcheung-code@users.noreply.github.com>
2025-05-17 13:40:39 -0700

    Improve navbar and brand logo styling for better responsiveness

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -9,11 +9,11 @@ import { BrandLogo } from "@/components/ui/BrandLogo";
 export function NavBar() {
     return (
         <header className="flex py-3 md:py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
-            <nav className="flex flex-col xs:flex-row items-center justify-between w-full px-2 sm:px-4 md:px-6 lg:container font-semibold">
-                <Link href="/" className="flex-shrink-0 mb-3 xs:mb-0">
+            <nav className="flex flex-row items-center justify-between w-full px-4 sm:px-4 md:px-6 lg:container font-semibold">
+                <Link href="/" className="flex-shrink-0 xs:mb-0">
                     <BrandLogo />
                 </Link>
-                <div className="flex justify-center gap-4 xs:gap-6 md:gap-8 lg:gap-10">
+                <div className="flex items-center justify-end gap-4 w-full xs:gap-6 md:gap-8 lg:gap-10">
                     <Link className="text-xs sm:text-sm md:text-base lg:text-lg whitespace-nowrap" href="#about">
                         About
                     </Link>
--- src/components/ui/BrandLogo.tsx
+++ src/components/ui/BrandLogo.tsx
@@ -1,7 +1,7 @@
 export function BrandLogo() {
     return (
-        <span className="flex items-center gap-2 font-semibold flex-shrink-0 text-lg">
-            <img src="/Tabpeek_logo_no_background.png" className="w-10 h-10" alt="TabPeek Logo" />
+        <span className="flex items-center gap-2 font-semibold flex-shrink-0 text-xl sm:text-3xl">
+            <img src="/Tabpeek_logo_no_background.png" className="w-12 h-12" alt="TabPeek Logo" />
             <span>TabPeek</span>
         </span>
     )
